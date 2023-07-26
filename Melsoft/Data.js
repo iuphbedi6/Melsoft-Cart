ffc35a8cc8115598d028909ee8f88c3e1702bf67
iuphbedi6 <136227491+iuphbedi6@users.noreply.github.com>
2023-07-26 02:20:41 +0200

    Add files via upload

---
+++ Melsoft/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Office Furniture",
+    price: 250000,
+    desc: "At Furniture IB we offfer the best office furniture.",
+    img: "image6.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Bathroom Furniture",
+    price: 100000,
+    desc: "We offer exquisite tailored designed bathroom sets.",
+    img: "image1.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Home Furniture",
+    price: 350000,
+    desc: "It's not just home furniture it's a match of the spirit.",
+    img: "image3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Kitchen Furniture",
+    price: 150000,
+    desc: "Kitchen sets that give birth to world class meals.",
+    img: "image4.jpg",
+  },
+];
